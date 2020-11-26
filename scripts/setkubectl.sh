d95f57484906c10816f8c9b82a40131645b211e3
ppresto <ppresto@hashicorp.com>
2020-11-25 23:00:57 -0800

    autounseal

--- scripts/setkubectl.sh
+++ scripts/setkubectl.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 GITDIR=/root/gcp-gke
-echo \${GOOGLE_CREDENTIALS} > ${GITDIR}/tmp/credential_key.json
-gcp_region=\$(terraform output -state=${GITDIR}/terraform.tfstate region)
-gcp_cluster_name=\$(terraform output -state=${GITDIR}/terraform.tfstate kubernetes_cluster_name)
+
+# Using zone for the region in tf makes smaller GKS footprint
+echo ${GOOGLE_CREDENTIALS} > ${GITDIR}/tmp/credential_key.json
+gcp_region=$(terraform output -state=${GITDIR}/terraform.tfstate region)
+gcp_cluster_name=$(terraform output -state=${GITDIR}/terraform.tfstate kubernetes_cluster_name)
 
 gcloud auth activate-service-account --key-file=${GITDIR}/tmp/credential_key.json
-gcloud config set project \${INSTRUQT_GCP_PROJECT_GCP_PROJECT_PROJECT_ID}
-gcloud config set compute/region \${gcp_region}
-gcloud container clusters get-credentials \${gcp_cluster_name} --region \${gcp_region}
+gcloud config set project ${INSTRUQT_GCP_PROJECT_GCP_PROJECT_PROJECT_ID}
+gcloud config set compute/region ${gcp_region}
+gcloud container clusters get-credentials ${gcp_cluster_name} --region ${gcp_region}
 
+# Create Kubernetes secret with the GOOGLE_CREDENTIALS for GCP Auto-Unseal.
 if [[ ! $(kubectl get secret kms-creds 2>/dev/null) ]]; then 
     kubectl create secret generic kms-creds --from-file=${GITDIR}/tmp/credential_key.json
     echo "Secret created to support Auto Unseal with GCP KMS"
