d730327807f5256f013310e53921658d11cd69fd
ppresto <ppresto@hashicorp.com>
2020-11-29 21:20:43 -0800

    chmod uninstall

--- scripts/uninstall_vault.sh
+++ scripts/uninstall_vault.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-release=$($(helm list -o json | jq -r '.[].name' | grep vault))
+release=$(helm list -o json | jq -r '.[].name' | grep vault)
 helm uninstall $release
 kubectl delete pvc -l app.kubernetes.io/name=vault
