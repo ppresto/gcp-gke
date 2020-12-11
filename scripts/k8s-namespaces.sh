ca9358a6057f1463da5b17f98d6ee0a5199e49cd
ppresto <ppresto@hashicorp.com>
2020-12-10 23:52:33 -0800

    clean up

--- scripts/k8s-namespaces.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-kubectl create namespace vault
-kubectl create namespace vaultdr
-
-helm install vault hashicorp/vault -f vault.yaml --namespace vault
-helm install vault hashicorp/vault -f vault.yaml --namespace vaultdr
